e9b0126faa591003d30d3238e1b08ce19e9a479a
Kwabena Osei <95602791+Kwabena2K@users.noreply.github.com>
2026-02-24 00:07:56 -0500

    text update 1

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -27,7 +27,8 @@ function ForgotPassword () {
     return (
         <div className="min-h-screen flex items-center justify-center bg-slate-800">
             <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-md">
-                <h1 className="mb-4 text-4xl text-black font-semibold text-center">Reset</h1>
+                <h1 className="mb-4 text-2xl text-black font-semibold text-center">RESET YOUR PASSWORD</h1>
+                <p className="mb-4 text-center">Enter the email address you used to register</p>
                 
                 <form  className="w-[90vw] max-w-96 flex flex-col gap-4">
                     {error && 
